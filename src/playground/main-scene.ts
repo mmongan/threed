77d678d09b0df9f1e5bcfff9db5821967d2e6abd
mmongan <41438677+mmongan@users.noreply.github.com>
2025-06-09 07:32:46 -0500

    Initial commit

---
+++ src/playground/main-scene.ts
@@ -0,0 +1,47 @@
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import { DefaultRenderingPipeline } from "@babylonjs/core/PostProcesses/RenderPipeline/Pipelines/defaultRenderingPipeline";
+import { Engine } from "@babylonjs/core/Engines/engine";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { Scene } from "@babylonjs/core/scene";
+import { Tools } from "@babylonjs/core/Misc/tools";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { WebGPUEngine } from "@babylonjs/core/Engines/webgpuEngine";
+// import { LoadAssetContainerAsync } from "@babylonjs/core/Loading/sceneLoader";
+import { Ground } from "./ground";
+
+export default class MainScene {
+  private camera: ArcRotateCamera;
+
+  constructor(private scene: Scene, private canvas: HTMLCanvasElement, private engine: Engine | WebGPUEngine) {
+    this._setCamera(scene);
+    this._setLight(scene);
+    //  this._setEnvironment(scene);
+    this.loadComponents();
+  }
+
+  _setCamera(scene: Scene): void {
+    this.camera = new ArcRotateCamera("camera", Tools.ToRadians(90), Tools.ToRadians(80), 20, Vector3.Zero(), scene);
+    this.camera.attachControl(this.canvas, true);
+    this.camera.setTarget(Vector3.Zero());
+  }
+
+  _setLight(scene: Scene): void {
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    light.intensity = 0.5;
+  }
+
+  _setEnvironment(scene: Scene) {
+    scene.createDefaultEnvironment({ createGround: false, createSkybox: false });
+  }
+
+  _setPipeLine(): void {
+    const pipeline = new DefaultRenderingPipeline("default-pipeline", false, this.scene, [this.scene.activeCamera!]);
+    pipeline.fxaaEnabled = true;
+    pipeline.samples = 4;
+  }
+
+  async loadComponents(): Promise<void> {
+    // Load your files in order
+    new Ground(this.scene);
+  }
+}
